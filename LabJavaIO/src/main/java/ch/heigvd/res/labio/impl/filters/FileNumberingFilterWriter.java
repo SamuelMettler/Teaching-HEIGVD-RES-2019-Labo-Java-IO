4be5ea693af51634a79d9a6e1bd5ea08d4d7749a
SamuelMettler <samuel@mettlerpm.com>
2019-03-17 10:05:02 +0100

    added some black magic to pass NumberingFilter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -4,38 +4,68 @@ import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class transforms the streams of character sent to the decorated writer.
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
  * process.
- *
+ * <p>
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private int compteur = 1;
+    private boolean isBr;
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
+        isBr = false;
+    }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        if(off + len > str.length()){
+            throw new IndexOutOfBoundsException("Longueur et offset ne sont pas corrects");
+        }
+        this.write(str.toCharArray(), off, len);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        if(off + len > cbuf.length){
+            throw new IndexOutOfBoundsException("Longueur et offset ne sont pas corrects");
+        }
+        for (int i = 0; i < len; ++i) {
+            write(cbuf[off + i]);
+        }
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(int c) throws IOException {
+        if (compteur == 1) {
+            super.write(String.valueOf(compteur++) + '\t');
+            super.write(c);
+        } else if ((char) c == '\n') {
+            isBr = false;
+            super.write(c);
+            super.write( String.valueOf(compteur++) + '\t');
 
+        } else if ((char) c == '\r') {
+            isBr = true;
+            super.write(c);
+        } else {
+            if (isBr) {
+                out.write(String.valueOf(compteur++) + '\t'); //si cette fonction est avec super.write cç ne fonctionne pas sur MacOS pour des raisons obscures
+                isBr = false;
+            }
+            super.write(c);
+        }
+    }
 }
+
